fa68a54403566114b80a110d9551c2d3156fea6f
dari862 <54928907+dari862@users.noreply.github.com>
2022-04-14 14:02:36 +0300

    Update theme_switch.sh

--- Config/polybar/base_scripts/theme_switch.sh
+++ Config/polybar/base_scripts/theme_switch.sh
@@ -7,13 +7,4 @@ RDIR="$HOME/.config/polybar/Rofi/$Theme_name"
 MENU="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p '' \
 -theme $RDIR/styles.rasi \
 <<< " forest| grayblocks| hack| material| pwidgets| shades| shapes| Titus")"
-            case "$MENU" in
-                                *forest) echo "forest" > "$PDIR"/Picked_Theme && "$PDIR"/launch.sh & ;; 
-                                *grayblocks) echo "grayblocks" > "$PDIR"/Picked_Theme && "$PDIR"/launch.sh & ;; 
-                                *hack) echo "hack" > "$PDIR"/Picked_Theme && "$PDIR"/launch.sh & ;; 
-                                *material) echo "material" > "$PDIR"/Picked_Theme && "$PDIR"/launch.sh & ;; 
-                                *pwidgets) echo "pwidgets" > "$PDIR"/Picked_Theme && "$PDIR"/launch.sh & ;; 
-                                *shades) echo "shades" > "$PDIR"/Picked_Theme && "$PDIR"/launch.sh & ;; 
-                                *shapes) echo "shapes" > "$PDIR"/Picked_Theme && "$PDIR"/launch.sh & ;; 
-                                *Titus) echo "Titus" > "$PDIR"/Picked_Theme && "$PDIR"/launch.sh & ;; 
-            esac
+echo "${MENU// }" > "$PDIR"/Picked_Theme && "$PDIR"/launch.sh &
