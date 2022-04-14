aa3f68bf4c2b5fcaac74f2d944b47e9374f649f7
dari862 <54928907+dari862@users.noreply.github.com>
2022-04-14 12:27:20 +0300

    Update WM.sh

--- core/WM.sh
+++ core/WM.sh
@@ -341,11 +341,11 @@ user1000=$(cat /etc/passwd | cut -f 1,3 -d: | grep :1000$ | cut -f1 -d:)
 # Change accent color blue (#5294e2) for grey:
 sudo find /usr/share/themes/Arc -type f -exec sed -i 's/#5294e2/#b3bcc6/g' {} \;   
 
-[ -d "/usr/share/icons/Numix-Paper" ] && rm -rf "/usr/share/icons/Numix-Paper"
-sudo tar -xzvf "$temp_folder_for_openbox"/numix-paper-icon-theme.tgz -C /usr/share/icons/	
+# install Tela-icon-theme
+$temp_folder_for_download/Tela-icon-theme/install.sh grey
 
 #sed -i 's/^gtk-theme-name *= *.*/gtk-theme-name="'"Arc"'"/' "$temp_folder_for_skel_/.config/gtk-2.0/gtkrc-2.0"
-#sed -i 's/^gtk-icon-theme-name *= *.*/gtk-icon-theme-name="'"Numix-Paper"'"/'	"$temp_folder_for_skel_/.config/gtk-2.0/gtkrc-2.0"
+#sed -i 's/^gtk-icon-theme-name *= *.*/gtk-icon-theme-name="'"Tela-grey"'"/'	"$temp_folder_for_skel_/.config/gtk-2.0/gtkrc-2.0"
 #sed -i 's/^gtk-cursor-theme-name *= *.*/gtk-cursor-theme-name="'"DMZ-White"'"/' "$temp_folder_for_skel_/.config/gtk-2.0/gtkrc-2.0"
 #sed -i 's/^gtk-toolbar-icon-size *= *.*/gtk-toolbar-icon-size="'"GTK_ICON_SIZE_SMALL_TOOLBAR"'"/' "$temp_folder_for_skel_/.config/gtk-2.0/gtkrc-2.0"
 #sed -i 's/^gtk-xft-hintstyle *= *.*/gtk-xft-hintstyle="'"hintslight"'"/' "$temp_folder_for_skel_/.config/gtk-2.0/gtkrc-2.0"
