801b903c7745008b6b19fb4d1512544eb6cd0a91
dari862 <54928907+dari862@users.noreply.github.com>
2022-04-15 17:35:09 +0300

    Update install_main_apps.sh

--- core/install_main_apps.sh
+++ core/install_main_apps.sh
@@ -8,7 +8,7 @@ extract_now()
       *.bz2)       bunzip2 $1   ;;
       *.rar)       unrar x $1     ;;
       *.gz)        gunzip $1    ;;
-      *.tar)       tar xf $1 -C $2 ;;f
+      *.tar)       tar xf $1 -C $2 ;;
       *.tbz2)      tar xjf $1 -C $2 ;;
       *.tgz)       tar xzf $1 -C $2 ;;
       *.zip)       unzip $1 -d $2 ;;
