06c8989cf5ecb487e751a56c500c7803fa0184eb
dari862 <54928907+dari862@users.noreply.github.com>
2022-04-16 02:10:38 +0300

    Update install_main_apps.sh

--- core/install_main_apps.sh
+++ core/install_main_apps.sh
@@ -299,49 +299,41 @@ conky_now
 
 install_polybar_app_now_()
 {
-if [ "$is_polybar_installed" != "true" ]
-then
-	show_m "install polybar app "
-	apt_if_install_whith_error2info "${install_polybar_[@]}"
-	mkdir -p $temp_folder_for_skel_config
-	cd $temp_folder_for_skel_config
-	svn-export https://github.com/dari862/my-linux-script/trunk/Config/polybar
-	show_m "install fonts for polybar app "
-	mkdir -p $temp_folder_for_download
-	sudo mkdir -p /usr/share/fonts
-	cd $temp_folder_for_download
-	svn-export $outsidemyrepo_fonts_polybar_themes
-	sudo chown -R root:root fonts
-	sudo mv $temp_folder_for_download/fonts/* /usr/share/fonts &> /dev/null || show_em "falied to move all fonts files"
-	sudo fc-cache -vf
-	declare -g is_polybar_installed="true"
-fi
+show_m "install polybar app "
+apt_if_install_whith_error2info "${install_polybar_[@]}"
+mkdir -p $temp_folder_for_skel_config
+cd $temp_folder_for_skel_config
+svn-export https://github.com/dari862/my-linux-script/trunk/Config/polybar
+show_m "install fonts for polybar app "
+mkdir -p $temp_folder_for_download
+sudo mkdir -p /usr/share/fonts
+cd $temp_folder_for_download
+svn-export $outsidemyrepo_fonts_polybar_themes
+sudo chown -R root:root fonts
+sudo mv $temp_folder_for_download/fonts/* /usr/share/fonts &> /dev/null || show_em "falied to move all fonts files"
+sudo fc-cache -vf
 }
 
 install_xfce4_panel_app_now_()
 {
-if [ "$is_xfce4_panel_installed" != "true" ]
-then
-	show_m "install xfce4-panel app "
-	echo_2_helper_list "# xfce4 apps"
-	apt_if_install_whith_error2info "${install_xfce4_panel[@]}"
-	echo_2_helper_list ""
-	mkdir -p $temp_folder_for_skel_config
-	cd $temp_folder_for_skel_config
-	svn-export https://github.com/dari862/my-linux-script/trunk/Config/xfce4-panel/xfce4
+show_m "install xfce4-panel app "
+echo_2_helper_list "# xfce4 apps"
+apt_if_install_whith_error2info "${install_xfce4_panel[@]}"
+echo_2_helper_list ""
+mkdir -p $temp_folder_for_skel_config
+cd $temp_folder_for_skel_config
+svn-export https://github.com/dari862/my-linux-script/trunk/Config/xfce4-panel/xfce4
 
-	###################################################################
-	show_m "Install clear xfce4-notify theme and configure xfce4-panel"
-	# Copy users config
-	sudo mkdir -p "/usr/share/themes/clear-notify/xfce-notify-4.0/"
-	mkdir -p $temp_folder_for_download
-	newwget -P $temp_folder_for_download https://raw.githubusercontent.com/dari862/my-linux-script/main/Config/xfce4-panel/clear_xfce-notify-4.0_gtk.css
-	sudo mv -v "$temp_folder_for_download/clear_xfce-notify-4.0_gtk.css" "/usr/share/themes/clear-notify/xfce-notify-4.0/gtk.css"
-	sudo chown root:root /usr/share/themes/clear-notify/xfce-notify-4.0/gtk.css
-	#fix xfce4-panel workspace settings error in openbox
-	sudo ln -s /usr/bin/obconf /usr/bin/xfwm4-workspace-settings
-	declare -g is_xfce4_panel_installed="true"
-fi
+###################################################################
+show_m "Install clear xfce4-notify theme and configure xfce4-panel"
+# Copy users config
+sudo mkdir -p "/usr/share/themes/clear-notify/xfce-notify-4.0/"
+mkdir -p $temp_folder_for_download
+newwget -P $temp_folder_for_download https://raw.githubusercontent.com/dari862/my-linux-script/main/Config/xfce4-panel/clear_xfce-notify-4.0_gtk.css
+sudo mv -v "$temp_folder_for_download/clear_xfce-notify-4.0_gtk.css" "/usr/share/themes/clear-notify/xfce-notify-4.0/gtk.css"
+sudo chown root:root /usr/share/themes/clear-notify/xfce-notify-4.0/gtk.css
+#fix xfce4-panel workspace settings error in openbox
+sudo ln -s /usr/bin/obconf /usr/bin/xfwm4-workspace-settings
 }
 
 ############################################################################
