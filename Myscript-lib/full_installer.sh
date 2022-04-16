a489e5fe7b612ce99489fafe2a573394a03b53fc
dari862 <54928907+dari862@users.noreply.github.com>
2022-04-16 17:18:23 +0300

    Update full_installer.sh

--- Myscript-lib/full_installer.sh
+++ Myscript-lib/full_installer.sh
@@ -47,7 +47,6 @@ fi
 
 if [ "$do_you_want_to_install_xfce4_panel" == "true" ]; then
 	install_xfce4_panel_app_now_
-	install_polybar_app_now_
 fi
 
 if [ "$do_you_want_to_install_awesomeWM" == "true" ]; then
