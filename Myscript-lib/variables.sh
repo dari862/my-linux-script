a861d050bcd0db821dbd98b2d3d2d305dd63a806
dari862 <54928907+dari862@users.noreply.github.com>
2022-05-01 16:57:47 +0300

    Update variables.sh

--- Myscript-lib/variables.sh
+++ Myscript-lib/variables.sh
@@ -180,7 +180,7 @@ wallpapers_location_now="/usr/share/my_wallpapers"
 gnome_wallpaper_folder="/usr/share/backgrounds"
 kde_wallpaper_folder="/usr/share/wallpapers"
 nitrogen_wp_default="nord1.png"
-replace_nitrogen_wp_default="openbox.png"
+replace_nitrogen_wp_default="default.jpg"
 
 # dotcleanup var
 is_zsh_default_shell="false"
