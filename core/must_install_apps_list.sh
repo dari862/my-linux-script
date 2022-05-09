b66d9cde01ecdf80a320ad7b62c071bfcf760c07
dari862 <54928907+dari862@users.noreply.github.com>
2022-05-09 18:10:27 +0300

    Update must_install_apps_list.sh

--- core/must_install_apps_list.sh
+++ core/must_install_apps_list.sh
@@ -35,8 +35,9 @@ Flatpak_apps_2_install=(com.discordapp.Discord com.notepadqq.Notepadqq)
 install_ca_certificates_=(ca-certificates)
 
 #preWM apps
-install_preWM_apps=(psmisc unzip git xorg pulseaudio dunst libnotify-bin xwallpaper arandr gnome-keyring dbus-x11 featherpad shotwell network-manager network-manager-gnome xprintidle)
+install_preWM_apps=(psmisc unzip git xorg pulseaudio dunst libnotify-bin arandr gnome-keyring dbus-x11 featherpad shotwell network-manager network-manager-gnome xprintidle rofi pnmixer pavucontrol)
 install_preWM_themes=(nitrogen picom lxpolkit lxappearance papirus-icon-theme fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls arc-theme gtk3-nocsd numix-icon-theme paper-icon-theme dmz-cursor-theme)
+install_preWM_if_laptop=(light upower)
 install_terminal_based_sound_app=(mpd mpc ncmpcpp)
 install_files_manager_app=(thunar thunar-archive-plugin thunar-media-tags-plugin catfish gvfs gvfs-fuse gvfs-backends file-roller)
 install_text_editer_app=(featherpad)
@@ -49,23 +50,23 @@ install_lightdm_extra=(lightdm-gtk-greeter lightdm-webkit2-greeter)
 
 #panel
 #libnm-dev for networkmanager_dmenu
-install_polybar_=(polybar rofi libnm-dev)
+install_polybar_=(polybar libnm-dev)
 install_xfce4_panel=( xfce4-panel xfce4-settings xfce4-screenshooter xfce4-clipman xfce4-notifyd xfce4-whiskermenu-plugin cbatticon)
 
 #awesomeWM apps
 install_awesomeWM_=(awesome awesome-extra)
-install_awesomeWM_extra=(xfce4-taskmanager xfce4-settings scrot flameshot nitrogen geany file-roller dmenu materia-gtk-theme)
+install_awesomeWM_extra=(xfce4-taskmanager xfce4-settings scrot flameshot geany file-roller dmenu materia-gtk-theme)
 
 #bspwm apps
 install_bspwm_=(bspwm sxhkd)
 install_bspwm_extra=(flameshot yad)
 
 #openbox apps
-install_openbox_=(openbox obconf xinit  xcape xautomation yad libnotify-bin pnmixer pavucontrol )
-install_openbox_extra=(rofi arandr gparted inxi s-tui dfc htop hwinfo cups synaptic )
+install_openbox_=(openbox obconf xinit  xcape xautomation yad )
+install_openbox_extra=(gparted inxi s-tui dfc htop hwinfo cups synaptic )
 install_openbox_extra2=(xdotool ntfs-3g acl evince galternatives mlocate mailutils mtp-tools gmtp)
 install_openbox_fonts=(fonts-noto fonts-droid-fallback fonts-cantarell fonts-liberation fonts-opensymbol fonts-noto-core fonts-noto-mono fonts-inconsolata)
 
 # ffmpeg for screen recoding , maim for ac-shots , python2 for ac-randr ac-kb ac-kb-pipemenu
-install_openbox_archcraft=(xmlstarlet xfce4-terminal xfce4-settings light upower qt5ct geany plank maim python2 ffmpeg qt5-style-kvantum)
+install_openbox_archcraft=(xmlstarlet qt5ct geany plank maim python2 ffmpeg qt5-style-kvantum)
 install_openbox_obmenu_generator=(obmenu-generator)
