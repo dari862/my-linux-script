bc31042c15beaeb4c251531e40ae3d9f8701457c
dari862 <54928907+dari862@users.noreply.github.com>
2022-05-09 04:19:46 +0300

    Update Mountain.sh

--- Config/xfce4-panel/xfce4/styles/Mountain.sh
+++ Config/xfce4-panel/xfce4/styles/Mountain.sh
@@ -30,9 +30,9 @@ change_rofi() {
 	/* Color-Scheme */
 
 	* {
-	    BG:    #212B30ff;
-	    FG:    #C4C7C5ff;
-	    BDR:   #EC407Aff;
+	    BG:    #1F252Bff;
+	    FG:    #F1FCF9ff;
+	    BDR:   #DB86BAff;
 	}
 	_EOF_
 }
