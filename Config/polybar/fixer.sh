7e624157f19a4f513257e8610dbfd321c6f08806
dari862 <54928907+dari862@users.noreply.github.com>
2022-05-23 13:02:06 +0300

    Update fixer.sh

--- Config/polybar/fixer.sh
+++ Config/polybar/fixer.sh
@@ -2,12 +2,66 @@
 
 ## Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
 ## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+## Files and Directories
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SFILE="$DIR/system.ini"
 
-Style="$1"
-DIR="${2}/$1"
-CARD="$(light -L | grep 'backlight' | head -n1 | cut -d'/' -f3)"
-INTERFACE="$(ip link | awk '/state UP/ {print $2}' | tr -d :)"
-RFILE="$DIR/.module"
+## create `system.ini` file
+create_system_file_ini() {
+cat << 'EOF' > $SFILE
+## Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+;; _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+;;
+;; System Variables (Edit according to your system)
+;;
+;; Warning : DO NOT DELETE THIS FILE
+;;
+;; Run `ls -1 /sys/class/power_supply/` to list list batteries and adapters.
+;;
+;; Run `ls -1 /sys/class/backlight/` to list available graphics cards.
+;;
+;; Run `ip link | awk '/state UP/ {print $2}' | tr -d :` to get active network interface.
+;;
+;; Polybar Variables For Modules :
+;; card = ${system.graphics_card}
+;; battery = ${system.battery}
+;; adapter = ACAD
+;; interface = ${system.network_interface}
+
+[system]
+adapter = ACAD
+battery = BAT1
+graphics_card = amdgpu_bl0
+network_interface = ens32
+
+;; _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+EOF
+}
+
+## Get system variable values for various modules
+get_values() {
+	CARD=$(light -L | grep 'backlight' | head -n1 | cut -d'/' -f3)
+	BATTERY=$(upower -i `upower -e | grep 'BAT'` | grep 'native-path' | cut -d':' -f2 | tr -d '[:blank:]')
+	ADAPTER=$(upower -i `upower -e | grep 'AC'` | grep 'native-path' | cut -d':' -f2 | tr -d '[:blank:]')
+	INTERFACE=$(ip link | awk '/state UP/ {print $2}' | tr -d :)
+}
+
+## Write values to `system.ini` file
+set_values() {
+	if [[ "$ADAPTER" ]]; then
+		sed -i -e "s/adapter = .*/adapter = $ADAPTER/g" 						${SFILE}
+	fi
+	if [[ "$BATTERY" ]]; then
+		sed -i -e "s/battery = .*/battery = $BATTERY/g" 						${SFILE}
+	fi
+	if [[ "$CARD" ]]; then
+		sed -i -e "s/graphics_card = .*/graphics_card = $CARD/g" 				${SFILE}
+	fi
+	if [[ "$INTERFACE" ]]; then
+		sed -i -e "s/network_interface = .*/network_interface = $INTERFACE/g" 	${SFILE}
+	fi
+}
 
 # Fix backlight and network modules
 fix_modules() {
@@ -22,27 +76,10 @@ fix_modules() {
 	fi
 }
 
-# Launch the bar
-launch_bar() {
-	# Terminate already running bar instances
-	killall -q polybar
+create_system_file_ini
+get_values
+set_values
 
-	# Wait until the processes have been shut down
-	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+fix_modules
 
-	# Launch the bar
-	if [ "$Style" == "hack" ]
-	then
-		polybar -q top -c "$DIR"/config.ini &
-		polybar -q bottom -c "$DIR"/config.ini &
-	else
-		polybar -q main -c "$DIR"/config.ini &
-	fi
-}
 
-# Execute functions
-if [[ ! -f "$RFILE" ]]; then
-	fix_modules
-	touch "$RFILE"
-fi	
-launch_bar
