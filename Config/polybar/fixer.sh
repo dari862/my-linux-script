b1246d25b3fb01257d63f5fcd85b93f64b3df351
dari862 <54928907+dari862@users.noreply.github.com>
2022-05-23 17:38:57 +0300

    Update fixer.sh

--- Config/polybar/fixer.sh
+++ Config/polybar/fixer.sh
@@ -67,12 +67,18 @@ set_values() {
 fix_modules() {
 	if [[ -z "$CARD" ]]; then
 		find $DIR -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ backlight / bna /g' {}/config.ini \;
+		find $DIR/panels -type f -exec sed -i -e 's/ backlight / bna /g' {} \;
+		find $DIR/pwidgets -type f -exec sed -i -e 's/ backlight / bna /g' {} \;
 	elif [[ "$CARD" != *"intel_"* ]]; then
 		find $DIR -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ backlight / brightness /g' {}/config.ini \;
+		find $DIR/panels -type f -exec sed -i -e 's/ backlight / brightness /g' {} \;
+		find $DIR/pwidgets -type f -exec sed -i -e 's/ backlight / brightness /g' {} \;
 	fi
 
 	if [[ "$INTERFACE" == e* ]]; then
 		find $DIR -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ network / ethernet /g' {}/config.ini \;
+		find $DIR/panels -type f -exec sed -i -e 's/ network / ethernet /g' {} \;
+		find $DIR/pwidgets -type f -exec sed -i -e 's/ network / ethernet /g' {} \;
 	fi
 }
 
