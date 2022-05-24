05b99e7bcb5da59b784b7ed3fafad4c0a8f21f7c
dari862 <54928907+dari862@users.noreply.github.com>
2022-05-24 07:54:45 +0300

    Update launch.sh

--- Config/polybar_rofi_extra/polybar_extra/launch.sh
+++ Config/polybar_rofi_extra/polybar_extra/launch.sh
@@ -45,3 +45,23 @@ if [[ ! -f "$RFILE" ]]; then
 	touch ${RFILE}
 fi
 launch_bar
+
+
+
+fix_modules() {
+	if [[ -z "$CARD" ]]; then
+		find $DIR -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ backlight / bna /g' {}/config.ini \;
+		find $DIR/panels -type f -exec sed -i -e 's/ backlight / bna /g' {} \;
+		find $DIR/pwidgets -type f -exec sed -i -e 's/ backlight / bna /g' {} \;
+	elif [[ "$CARD" != *"intel_"* ]]; then
+		find $DIR -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ backlight / brightness /g' {}/config.ini \;
+		find $DIR/panels -type f -exec sed -i -e 's/ backlight / brightness /g' {} \;
+		find $DIR/pwidgets -type f -exec sed -i -e 's/ backlight / brightness /g' {} \;
+	fi
+
+	if [[ "$INTERFACE" == e* ]]; then
+		find $DIR -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ network / ethernet /g' {}/config.ini \;
+		find $DIR/panels -type f -exec sed -i -e 's/ network / ethernet /g' {} \;
+		find $DIR/pwidgets -type f -exec sed -i -e 's/ network / ethernet /g' {} \;
+	fi
+}
