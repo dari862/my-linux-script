45f063e192363ac20500b1ddca597501414958c2
dari862 <54928907+dari862@users.noreply.github.com>
2022-05-27 12:54:17 +0300

    Update fixer.sh

--- Config/polybar/fixer.sh
+++ Config/polybar/fixer.sh
@@ -10,8 +10,9 @@ if [[ "$Laptop_Name" ]]; then
 fi
 
 ## Files and Directories
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-SFILE="$DIR/system.ini"
+P_Dir="$HOME/.config/polybar"
+PE_Dir="$HOME/.config/polybar_extra"
+SFILE="$P_Dir/system.ini"
 
 ## create `system.ini` file
 create_system_file_ini() {
@@ -93,23 +94,45 @@ set_values() {
 # Fix backlight and network modules
 fix_modules() {
 	if [[ -z "$CARD" ]]; then
-		find $DIR -mindepth 1 -type d -not -name 'scripts' -exec sed -i -e 's/ backlight / bna /g' {}/config.ini \;
+		find $P_Dir -mindepth 1 -type d -not -name 'scripts' -exec sed -i -e 's/ backlight / bna /g' {}/config.ini \;
 	elif [[ "$CARD" != *"intel_"* ]] || [[ "$Laptop_Thinkpad_X1" ]] ; then
-		find $DIR -mindepth 1 -type d -not -name 'scripts' -exec sed -i -e 's/ backlight / brightness /g' {}/config.ini \;
+		find $P_Dir -mindepth 1 -type d -not -name 'scripts' -exec sed -i -e 's/ backlight / brightness /g' {}/config.ini \;
 	fi
 
 	if [[ "$INTERFACE" == e* ]]; then
-		find $DIR -mindepth 1 -type d -not -name 'scripts' -exec sed -i -e 's/ network / ethernet /g' {}/config.ini \;
+		find $P_Dir -mindepth 1 -type d -not -name 'scripts' -exec sed -i -e 's/ network / ethernet /g' {}/config.ini \;
 	fi
 	
 	if [[ "$(cat /sys/class/dmi/id/chassis_type)" != @(8|9|10|14) ]]; then
-		find $DIR -mindepth 1 -type d -not -name 'scripts' -exec sed -i -e 's/ battery / AC_only /g' {}/config.ini \;
+		find $P_Dir -mindepth 1 -type d -not -name 'scripts' -exec sed -i -e 's/ battery / AC_only /g' {}/config.ini \;
 	fi
+	
+	if [[ -d "$PE_Dir" ]]; then
+		if [[ -z "$CARD" ]]; then
+			find $PE_Dir -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ backlight / bna /g' {}/config.ini \;
+			find $PE_Dir/panels -type f -exec sed -i -e 's/ backlight / bna /g' {} \;
+			find $PE_Dir/pwidgets -type f -exec sed -i -e 's/ backlight / bna /g' {} \;
+		elif [[ "$CARD" != *"intel_"* ]]; then
+			find $PE_Dir -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ backlight / brightness /g' {}/config.ini \;
+			find $PE_Dir/panels -type f -exec sed -i -e 's/ backlight / brightness /g' {} \;
+			find $PE_Dir/pwidgets -type f -exec sed -i -e 's/ backlight / brightness /g' {} \;
+		fi
+	
+		if [[ "$INTERFACE" == e* ]]; then
+			find $PE_Dir -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ network / ethernet /g' {}/config.ini \;
+			find $PE_Dir/panels -type f -exec sed -i -e 's/ network / ethernet /g' {} \;
+			find $PE_Dir/pwidgets -type f -exec sed -i -e 's/ network / ethernet /g' {} \;
+		fi
+		
+		if [[ "$(cat /sys/class/dmi/id/chassis_type)" != @(8|9|10|14) ]]; then
+			find $PE_Dir -mindepth 1 -type d -not -name 'scripts' -not -name 'panels' -not -name 'pwidgets' -exec sed -i -e 's/ battery / AC_only /g' {}/config.ini \;
+			find $PE_Dir/panels -type f -exec sed -i -e 's/ battery / AC_only /g' {} \;
+			find $PE_Dir/pwidgets -type f -exec sed -i -e 's/ battery / AC_only /g' {} \;
+		fi
+	fi	
 }
 
 create_system_file_ini
 get_values
 set_values
 fix_modules
-
-
