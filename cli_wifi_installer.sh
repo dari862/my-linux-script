76c28e6a98855cec83271a3ed5f8c65ef2ec35a6
dari862 <54928907+dari862@users.noreply.github.com>
2022-06-12 19:17:10 +0300

    Update cli_wifi_installer.sh

--- cli_wifi_installer.sh
+++ cli_wifi_installer.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e
-[ "$(id -u)" -ne 0 ] && { echo "sudo not installed, so you must run script as root" 1>&2; exit 1; }
 
-wifi_interface="$(ip link | awk -F: '$0 !~ "^[^0-9]"{print $2;getline}' | awk '/w/{ print $0 }')"
-
-if [ -z "$wifi_interface" ]
-then
-	echo "no wifi interface"
-	exit 1
-fi
-
-ip link set $wifi_interface up
-
-if command -v wpa_supplicant &> /dev/null
-then
+run_wpa_supplicant_now()
+{
 	read -p "ssid:" ssid_var
 	read -p "pass:" pass_var 
 	wpa_passphrase "$ssid_var" "$pass_var" | tee wpa_passphrase_output
@@ -26,20 +15,50 @@ then
 	apt-get update
 	apt-get install -y network-manager
 	killall wpa_supplicant
-	systemctl start NetworkManager.service 
-	systemctl enable NetworkManager.service
 	nmcli dev wifi connect network-ssid password "network-password"
-elif command -v nmcli &> /dev/null
+}
+
+run_nmcli_now()
+{
 	nmcli radio wifi on
 	while :
 	do
 		nmcli --ask dev wifi connect && break
 	fi
-fi
+}
 
+install_my_linux_script_now()
+{
 if command -v curl &> /dev/null
 then
 	bash <(curl -s https://raw.githubusercontent.com/dari862/my-linux-script/main/installer.sh)
 elif command -v wget &> /dev/null
 	bash <(wget -q -O - https://raw.githubusercontent.com/dari862/my-linux-script/main/installer.sh)
 fi
+}
+
+main()
+{
+	[ "$(id -u)" -ne 0 ] && { echo "sudo not installed, so you must run script as root" 1>&2; exit 1; }
+
+	wifi_interface="$(ip link | awk -F: '$0 !~ "^[^0-9]"{print $2;getline}' | awk '/w/{ print $0 }')"
+
+	if [ -z "$wifi_interface" ]
+	then
+		echo "no wifi interface"
+		exit 1
+	fi
+
+	ip link set $wifi_interface up
+	
+	if command -v wpa_supplicant &> /dev/null
+	then
+		run_wpa_supplicant_now
+	elif command -v nmcli &> /dev/null
+	then
+		run_nmcli_now
+	fi
+	install_my_linux_script_now
+}
+
+main
