cff27e2a67c4a80e44e81367914acceb33848e1e
dari862 <54928907+dari862@users.noreply.github.com>
2022-06-12 22:25:21 +0300

    Update dotfile_cleanup.sh

--- core/dotfile_cleanup.sh
+++ core/dotfile_cleanup.sh
@@ -63,25 +63,6 @@ cat << 'eof' > $temp_folder_for_skel_shell_folder/profile
 # profile file. Runs on login. Environmental variables are set here.
 
 # set tty colours.
-if [ "$TERM" = "linux" ]; then
-    printf "\e]P0000000" # color0
-    printf "\e]P19e1828" # color1
-    printf "\e]P2aece92" # color2
-    printf "\e]P3968a38" # color3
-    printf "\e]P4414171" # color4
-    printf "\e]P5963c59" # color5
-    printf "\e]P6418179" # color6
-    printf "\e]P7bebebe" # color7
-    printf "\e]P8888888" # color8
-    printf "\e]P9cf6171" # color9
-    printf "\e]PAc5f779" # color10
-    printf "\e]PBfff796" # color11
-    printf "\e]PC4186be" # color12
-    printf "\e]PDcf9ebe" # color13
-    printf "\e]PE71bebe" # color14
-    printf "\e]PFffffff" # color15
-#   clear # removes artefacts but also removes /etc/{issue,motd}
-fi
 
 # Default programs:
 export EDITOR="nano"
