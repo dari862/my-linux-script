67966e1126e7024771b3c8ebe333a09b3816ecb6
dari862 <54928907+dari862@users.noreply.github.com>
2022-06-17 18:02:40 +0300

    Update hotplug_usr.sh

--- Config/udev_rules/bin/hotplug_usr.sh
+++ Config/udev_rules/bin/hotplug_usr.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 if [ "$(ls -A ~/.screenlayout)" ] 
-	then
-		"$(ls ~/.screenlayout/*.sh | head -1)"	
-	elif [ "$(ls -A ~/.local/screenlayout)" ]
-	then
-		case "$(xrandr -q | grep "connected" | awk '/ connected/ {print $1}' | wc -l)" in
-			1) ~/.local/screenlayout/singale.sh ;;
-			2) ~/.local/screenlayout/multi-monitor.sh ;;
-			*) ~/.local/screenlayout/multi-monitor.sh ;;
-		esac
-	else
+then
+	"$(ls ~/.screenlayout/*.sh | head -1)"	
+elif [ "$(ls -A ~/.local/screenlayout)" ]
+then
+	case "$(xrandr -q | grep "connected" | awk '/ connected/ {print $1}' | wc -l)" in
+		1) ~/.local/screenlayout/singale.sh ;;
+		2) ~/.local/screenlayout/multi-monitor.sh ;;
+		*) ~/.local/screenlayout/multi-monitor.sh ;;
+	esac
+else
 	~/.local/bin/hub displayselect auto
-	fi
+fi
