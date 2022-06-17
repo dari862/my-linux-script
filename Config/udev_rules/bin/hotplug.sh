e0112713142781a1d88ee0f7afdd233c56086dbe
dari862 <54928907+dari862@users.noreply.github.com>
2022-06-17 18:15:35 +0300

    Update hotplug.sh

--- Config/udev_rules/bin/hotplug.sh
+++ Config/udev_rules/bin/hotplug.sh
@@ -4,7 +4,7 @@ EXTERNAL="HDMI1"
 export DISPLAY=":0"
 User_=$(who | grep $DISPLAY | head -1 | cut -f 1 -d ' ')
 sleep 1
-INTERNAL=$(su $User_ -c "xrandr --current | head -2 | tail -1 | awk '{print $1}'")
+INTERNAL=$(su $User_ -c "xrandr --current | head -2 | tail -1 | cut -d ' ' -f1")
 
 INTERNAL_STATUS=$(su $USER -c "xrandr --current | grep $INTERNAL | cut -d \  -f 2")
 EXTERNAL_STATUS=$(su $USER -c "xrandr --current | grep $EXTERNAL | cut -d \  -f 2")
