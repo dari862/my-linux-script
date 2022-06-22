779207b1f5dfdbeb659b9a90e185969d694d3aeb
dari862 <54928907+dari862@users.noreply.github.com>
2022-06-22 11:11:42 +0300

    Update common.sh

--- core/common.sh
+++ core/common.sh
@@ -168,3 +168,21 @@ disable_11n_now_() {
   fi
   echo "802.11n networking disabled in ${iwlwificonf}."
 }
+
+fixed_libxft_library_now() {
+	# Install fixed libxft library to /usr/lib/
+	cd /tmp/
+	git clone https://github.com/uditkarode/libxft-bgra
+	cd libxft-bgra/
+	sh autogen.sh --sysconfdir=/etc --prefix=/usr --mandir=/usr/share/man
+	sudo make install
+
+	# Link fixed files
+	sudo rm /usr/lib/x86_64-linux-gnu/libXft.so.2.3.3 || true
+	sudo rm /usr/lib/x86_64-linux-gnu/libXft.so.2 || true
+	sudo rm /usr/lib/x86_64-linux-gnu/libXft.so || true
+
+	sudo ln -s /usr/lib/libXft.so.2.3.3 /usr/lib/x86_64-linux-gnu/libXft.so.2.3.3
+	sudo ln -s /usr/lib/libXft.so.2.3.3 /usr/lib/x86_64-linux-gnu/libXft.so.2
+	sudo ln -s /usr/lib/libXft.so.2.3.3 /usr/lib/x86_64-linux-gnu/libXft.so
+}
